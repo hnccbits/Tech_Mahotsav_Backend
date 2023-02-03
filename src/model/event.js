f144dd2339392e1768e2ba11bc7182152191d7ed
akm-akm <adityanick08@gmail.com>
2023-02-03 17:15:28 +0530

    good

--- src/model/event.js
+++ src/model/event.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-
+const multer = require('multer');
 const eventSchema = new mongoose.Schema(
   {
     name: {
@@ -96,51 +96,4 @@ const eventSchema = new mongoose.Schema(
 const Event = mongoose.model("Event", eventSchema);
 module.exports = Event;
 
-/*
- {
-        participant: [
-          {
-            name: {
-              type: String,
-              required: true,
-              trim: true,
-              minlength: 3,
-              maxlength: 100
-            },
-            email: {
-              type: String,
-              required: true,
-              trim: true,
-              lowercase: true,
-              validate(value) {
-                if (!validator.isEmail(value)) {
-                  throw new Error("Email is invalid");
-                }
-              }
-            },
-            phone: {
-              required: true,
-              type: String,
-              maxlength: 10,
-            },
-            whatsapp: {
-              maxlength: 10,
-              type: String,
-              required: true
-            }
-          }
-        ],
-        captain: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User"
-        },
-        teamname: {
-          type: String,
-          required: true,
-          unique: true,
-          trim: true,
-          minlength: 3,
-          maxlength: 100
-        }
-      }
-      */
+
--- src/model/mongoose.js
+++ src/model/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
