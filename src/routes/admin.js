4b405acceeadd837b6054c8da48dfba7177fc8df
akm-akm <adityanick08@gmail.com>
2023-02-04 19:37:27 +0530

    all good

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -100,20 +100,25 @@ router.patch("/admin/update/event", admin, async (req, res) => {
     const { name, desc, teamsize, id: _id } = req.body;
 
     const event = await Event.findById({ _id });
+
     if (!event) throw new Error("Invalid Event id");
+    const { user } = req;
+    const { name: x } = user;
+
+    if (event.club != x) throw new Error("Unautharized");
     Event.uploadFile(req, res, async (err) => {
       if (err) throw new Error("Multer Error");
-      const { user } = req;
+
       const { name: club } = user;
       event.name = name;
       event.teamsize = teamsize;
       event.club = club;
       event.desc = desc;
-      if (req.files.coverimg[0].blobName) {
+      if (req.files.coverimg[0]?.blobName) {
         const coverimg = req.files.coverimg[0].blobName;
         event.coverimg = coverimg;
       }
-      if (req.files.rulebook[0].blobName) {
+      if (req.files.rulebook[0]?.blobName) {
         const rulebook = req.files.rulebook[0].blobName;
         event.rulebook = rulebook;
       }
@@ -129,9 +134,9 @@ router.delete("/admin/delete/event", admin, async (req, res) => {
   try {
     const { _id } = req.body;
     const { user } = req;
-    const { name: names } = user;
+    const { name } = user;
     const event = await Event.findById({ _id });
-    if (event.club != names) throw new Error("Unautharized");
+    if (event.club != name) throw new Error("Unautharized");
     await Event.findByIdAndDelete({ _id });
     res.status(201).json({ data: "success" });
   } catch ({ message }) {
